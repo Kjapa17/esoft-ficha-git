2dd16c172ed7adf79a156a2fd826e89de07928fc
Rúben Ferreira <rubenferreira519@gmail.com>
2024-02-28 10:47:15 +0000

    adicionar o meu nome

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,9 +3,11 @@ public class HelloWorld extends JFrame {
     private JPanel panel1;
 
     private JLabel lblMensagem;
+    private JLabel lblMensagem2;
     public HelloWorld(){
         setContentPane(panel1);
         lblMensagem = new JLabel("Hello World", SwingConstants.CENTER);
+        lblMensagem2 = new JLabel("Rubão passou por aqui", SwingConstants.CENTER);
         setContentPane(lblMensagem);
         pack();
     }
